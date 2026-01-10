8edcfb423c5b8776d0f5c30f8151e991b4b8da06
Sibananda Pradhan <anand7978085510@gmail.com>
2026-01-11 00:09:10 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,8 @@ output ec2_public_ips {
 output "ec2_private_ips" {
     value = aws_instance.my_instance.*.private_ip   # * use for find public and private IP for multiple instance count=3
 }
+
+#Use this output block if we are usinng for each in resource block for creating multiple instances
+output "ec2_public_ip" {
+    value = [for key, instance in aws_instance.my_instance : key => instance.public_ip]
+}
