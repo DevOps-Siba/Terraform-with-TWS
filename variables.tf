e69a63dd26ddcec49681b2e79d8b97ebc13b303d
Sibananda Pradhan <anand7978085510@gmail.com>
2026-01-11 00:10:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,3 +17,13 @@ variable "my_enviroment" {
   description = "Instance type for the EC2 instance"
   default     = "dev"
 }
+
+#ensure when we use local block for multiple instance we should ensure that the count =3 is removed for the resource block
+locals {  
+  instances = {
+      "key" = "instace_1"
+      "key2" = "instance_2"
+
+  } 
+}
+
